ec6b15fc1683c806f99bc1eb29a9e6864898791c
Ilman Sunanuddin <ilmans@Ilmans.local>
2022-12-11 15:18:54 +0700

    add filter for questions

--- model.js
+++ model.js
@@ -52,12 +52,13 @@ const botAi = async (req,res) => {
             } else {
                 const response = await openai.createCompletion({
                     model: "text-davinci-003",
-                    prompt: message,
-                    temperature: 0.3, 
+                    prompt: "Q : " + message,
+                    temperature: 0, 
                     max_tokens: 300,
-                    top_p: 1.0,
+                    top_p: 1,
                     frequency_penalty: 0.0,
                     presence_penalty: 0.0,
+                    stop: ["\n"],
                   
                 });
         
