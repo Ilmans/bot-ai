a3aaa57e15af476763da288d8b10a73b6079cdf2
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2022-12-13 12:26:33 +0700

    re factor

--- model.js
+++ model.js
@@ -1,41 +1,40 @@
-const {Sticker,createSticker,StickerTypes} = require('wa-sticker-formatter');
+const fs = require('fs');
+const { Sticker, StickerTypes } = require('wa-sticker-formatter');
 const { Configuration, OpenAIApi } = require("openai");
+const { removeContact, checkContact, saveContact } = require('./helpers')
+require('dotenv').config();
+
 const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const fs = require('fs');
 const botAi = async (req,res) => {
     const {message,bufferImage,from} = req.body;
 
-    let sip = false;
+    let response = false;
+    const isContactExist = await checkContact(from)
     if (message == '/start'){
-          const lk = await checkContact(from);
-          if(lk == false) {
+        if (isContactExist == false) {
               saveContact(from)
         } 
-        sip = { text: 'Bot telah aktif ,silahkan dimanfaatkan :)' }
+        response = { text: 'Bot telah aktif ,silahkan dimanfaatkan :)' }
     } else if (message == '/stop' ) {
-        const lk = await checkContact(from);
-        if (lk == true) {
+        if (isContactExist == true) {
             removeContact(from)
         }
-        sip = { text: 'Bot telah dihentikan,,terimakasih :)' }
+        response = { text: 'Bot telah dihentikan,,terimakasih :)' }
     } else {
-
-        const check = await checkContact(from);
-        if(check){
-
-            if (bufferImage) {
-                if (message == '/stiker') {
+        if (!isContactExist) return;
+        if (message == '/stiker') {
+            if (!bufferImage) return;
                     const buffer = Buffer.from(bufferImage,'base64');
                     fs.writeFileSync('./public/images/testing.png',buffer)
                     if(fs.existsSync('./public/images/testing.webp')){
                        fs.unlinkSync('./public/images/testing.webp')
                     }
-                    const sticker = new Sticker('http://localhost:3400/images/testing.png', {
+            const sticker = new Sticker(`${process.env.BASE_URL}/images/testing.png`, {
                         pack: 'MPedia Pack', // The pack name
                         author: 'M Pedia', // The author name
                         type: StickerTypes.CROPPED,
@@ -46,11 +45,10 @@ const botAi = async (req,res) => {
                     })
                     const k = await sticker.toFile('./public/images/testing.webp');
             
-                   sip = {sticker : {url : 'http://localhost:3400/images/testing.webp'}}
-                }
+            response = { sticker: { url: `${process.env.BASE_URL}/images/testing.webp` } }
                 
             } else {
-                const response = await openai.createCompletion({
+            const getResponse = await openai.createCompletion({
                     model: "text-davinci-003",
                     prompt: message,
                     temperature: 0, 
@@ -61,64 +59,14 @@ const botAi = async (req,res) => {
                   
                 });
         
-               const res = response.data.choices[0].text;
-              
-               sip = {text : res}
-        
-        
-            }
+            response = { text: getResponse.data.choices[0].text }
         }
+
     }
-
-
-
-   res.json(sip)
+    res.json(response)
 }
 
 
-
-const loadContact = async () => {
-    const fileBuffer = fs.readFileSync('datakontak.json', 'utf-8');
-    const contacts = JSON.parse(fileBuffer);
-    return contacts;
-}
-
-const saveContact = async (from) => {
-    const contact = { from };
-  
-    const contacts = await loadContact();
-    contacts.push(contact);
-    fs.writeFileSync('datakontak.json', JSON.stringify(contacts));
-}
-
-const checkContact = async (from) => {
-   
-    const contacts =  await loadContact();
-    const contact =  contacts.find(
-        (contact) => contact.from === from
-    )
-
-   if(!contact) {
-    return false;
-   }
-   return true;
-
-}
-
-const removeContact = async (from)   => {
-   
-    const contacts = await loadContact();
-   
-    const contactsNew = contacts.filter(
-        (contact) => contact.from != from
-    )
-    console.log(contactsNew);
- 
-   
-
-    fs.writeFileSync('datakontak.json',JSON.stringify(contactsNew));
-   
-}
 module.exports = {
     botAi
 }
